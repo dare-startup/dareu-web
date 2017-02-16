bffc5ea2e305d410d36753fb385a2d943e5c8ed5
Alberto <arubalcaba@24hourfit.com>
2017-02-16 15:27:12 -0800

    Updated saved date timezone

--- src/main/java/com/dareu/web/core/DareUtils.java
+++ src/main/java/com/dareu/web/core/DareUtils.java
@@ -3,6 +3,7 @@ package com.dareu.web.core;
 import java.math.BigInteger;
 import java.security.SecureRandom;
 import java.text.SimpleDateFormat;
+import java.util.TimeZone;
 
 import javax.ejb.Stateless;
 
@@ -12,6 +13,10 @@ public class DareUtils {
 	public static final SimpleDateFormat DATE_FORMAT = new SimpleDateFormat("MM/dd/YY");
         public static final SimpleDateFormat DETAILS_DATE_FORMAT = new SimpleDateFormat("MM/dd/yyyy HH:mm");
         
+        static{
+            DATE_FORMAT.setTimeZone(TimeZone.getTimeZone("PST"));
+            DETAILS_DATE_FORMAT.setTimeZone(TimeZone.getTimeZone("PST"));
+        }
 	private SecureRandom random = new SecureRandom();
 	
 	public DareUtils(){
